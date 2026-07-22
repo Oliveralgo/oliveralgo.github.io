23231a928b7edd3d2128d4ab23b8b7a49c7a6867
AgasKhan <lucas_galardo_8@yahoo.com.ar>
2026-07-22 06:52:54 -0300

    [New] game.html

--- script.js
+++ script.js
@@ -4,12 +4,24 @@
 //   hace que las cosas reaccionen y se muevan.
 // ============================================
 
+
+function mostrarMensaje()
+{
+  // Elegimos una frase al azar de la lista
+  const indiceAlAzar = Math.floor(Math.random() * frases.length);
+
+  // La mostramos en el párrafo con id="mensaje"
+  mensaje.textContent = frases[indiceAlAzar];
+}
+
+
 // 1) Buscamos los elementos del HTML por su "id"
 const boton = document.getElementById("boton");
 const mensaje = document.getElementById("mensaje");
 
 // 2) Una lista de frases para mostrar al azar
-const frases = [
+const frases = 
+[
   "¡Muy bien! 🎉",
   "¡Estás aprendiendo a programar! 👏",
   "¡Sos una leyenda! 🌟",
@@ -17,14 +29,9 @@ const frases = [
   "¡Qué buen clic! 😎",
 ];
 
-// 3) Cuando se hace clic en el botón, ejecutamos esta función
-boton.addEventListener("click", function () {
-  // Elegimos una frase al azar de la lista
-  const indiceAlAzar = Math.floor(Math.random() * frases.length);
 
-  // La mostramos en el párrafo con id="mensaje"
-  mensaje.textContent = frases[indiceAlAzar];
-});
+// 3) Cuando se hace clic en el botón, ejecutamos esta función
+boton.addEventListener("click", mostrarMensaje);
 
 // Mensaje de bienvenida en la consola del navegador (F12)
 console.log("¡La página cargó correctamente! 🎈");
